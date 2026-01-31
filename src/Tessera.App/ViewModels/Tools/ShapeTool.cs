f73bd516cb2ae546b6b6a461b51103d9c86e8893
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-01-31 18:28:15 +0100

    Adjust ShapeTool to use transform matrix

--- src/Tessera.App/ViewModels/Tools/ShapeTool.cs
+++ src/Tessera.App/ViewModels/Tools/ShapeTool.cs
@@ -20,7 +20,9 @@ public class ShapeTool : ICanvasTool
     }
     public void OnPointerPressed(Point p)
     {
-        _startPoint  = p;
+        if (!_vm.ViewMatrix.HasInverse) return;
+        
+        _startPoint = p.Transform(_vm.ViewMatrix.Invert());
         _previewShape = CreateShape(_settings.SelectedShapeType);
         _previewShape.X = p.X;
         _previewShape.Y = p.Y;
@@ -36,11 +38,14 @@ public class ShapeTool : ICanvasTool
     public void OnPointerMoved(Point p)
     {
         if (_previewShape == null) return;
+        if (!_vm.ViewMatrix.HasInverse) return;
         
-        var x = Math.Min(p.X, _startPoint.X);
-        var y = Math.Min(p.Y, _startPoint.Y);
-        var w = Math.Abs(p.X - _startPoint.X);
-        var h = Math.Abs(p.Y - _startPoint.Y);
+        var currentPoint = p.Transform(_vm.ViewMatrix.Invert());
+        
+        var x = Math.Min(currentPoint.X, _startPoint.X);
+        var y = Math.Min(currentPoint.Y, _startPoint.Y);
+        var w = Math.Abs(currentPoint.X - _startPoint.X);
+        var h = Math.Abs(currentPoint.Y - _startPoint.Y);
 
         _previewShape.X = x;
         _previewShape.Y = y;
