d6c159b73a9c5bf4699ce1ad4b3e27a03545a772
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-01-31 18:17:49 +0100

    Adjust LineShapeTool to use transform matrix

--- src/Tessera.App/ViewModels/Tools/LineShapeTool.cs
+++ src/Tessera.App/ViewModels/Tools/LineShapeTool.cs
@@ -6,35 +6,47 @@ namespace Tessera.App.ViewModels;
 
 public class LineShapeTool : ICanvasTool
 {
-    private readonly DrawingPageViewModel _drawingPageViewModel;
+    private readonly DrawingPageViewModel _vm;
     private readonly LineShapeToolSettings _settings;
     
     private LineShape? _line;
     
     public LineShapeTool(DrawingPageViewModel  drawingPageViewModel, LineShapeToolSettings settings)
     {
-        _drawingPageViewModel = drawingPageViewModel;
+        _vm = drawingPageViewModel;
         _settings = settings;
     }
     
     public void OnPointerPressed(Point p)
     {
+        if (!_vm.ViewMatrix.HasInverse)
+            return;
+        
+        var inverse = _vm.ViewMatrix.Invert();
+        var worldPoint = p.Transform(inverse);
+        
         _line = new LineShape
         {
-            StartPoint = new Point(p.X, p.Y),
-            EndPoint = new Point(p.X, p.Y),
+            StartPoint = new Point(worldPoint.X, worldPoint.Y),
+            EndPoint = new Point(worldPoint.X, worldPoint.Y),
             StrokeThickness = _settings.StrokeThickness,
             Color = new SolidColorBrush(_settings.StrokeColor),
         };
-        
-        _drawingPageViewModel.Shapes.Add(_line);
+
+        _vm.Shapes.Add(_line);
     }
 
     public void OnPointerMoved(Point p)
     {
         if (_line == null) return;
         
-        _line.EndPoint = new Point(p.X, p.Y);
+        if (!_vm.ViewMatrix.HasInverse)
+            return;
+        
+        var inverse = _vm.ViewMatrix.Invert();
+        var worldPoint = p.Transform(inverse);
+        
+        _line.EndPoint = new Point(worldPoint.X, worldPoint.Y);
     }
 
     public void OnPointerReleased(Point p)
