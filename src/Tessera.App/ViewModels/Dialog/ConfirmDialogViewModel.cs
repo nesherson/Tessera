c3c8a5d840a45576331efedfe7e9b090e546667e
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-02-03 01:12:26 +0100

    Add confirm dialog

---
+++ src/Tessera.App/ViewModels/Dialog/ConfirmDialogViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Tessera.App.ViewModels;
+
+public partial class ConfirmDialogViewModel : DialogViewModelBase
+{
+    [ObservableProperty]
+    private string _title = "Confirm";
+    [ObservableProperty]
+    private string _message = "Are you sure you want to continue?";
+    [ObservableProperty]
+    private string _confirmText = "Yes";
+    [ObservableProperty]
+    private string _cancelText = "No";
+    [ObservableProperty]
+    private string _iconPath = "/Assets/Icons/info.svg";
+    [ObservableProperty]
+    private bool _confirmed;
+    
+    [RelayCommand]
+    private void Confirm()
+    {
+        Confirmed = true;
+        
+        Close();
+    }
+    
+    [RelayCommand]
+    private void Cancel()
+    {
+        Confirmed = false;
+        
+        Close();
+    }
+}
---
+++ src/Tessera.App/ViewModels/Dialog/DialogViewModelBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Tessera.App.ViewModels;
+
+public partial class DialogViewModelBase : ViewModelBase
+{
+    [ObservableProperty]
+    private bool _isOpen;
+    [ObservableProperty]
+    private double _dialogWidth = double.NaN;
+
+    protected TaskCompletionSource CloseTask = new();
+    
+    public async Task WaitAsync()
+    {
+        await CloseTask.Task;
+    }
+
+    public void Show()
+    {
+        if (CloseTask.Task.IsCompleted)
+            CloseTask = new TaskCompletionSource();
+        
+        IsOpen = true;
+    }
+
+    public void Close()
+    {
+        IsOpen = false;
+        
+        CloseTask.TrySetResult();
+    }
+    
+    // public void ShowWindowNotifcation(string title, string message, NotificationType type, Action? onClick = null)
+    // {
+    //     WeakReferenceMessenger.Default
+    //         .Send(new ShowWindowNotificationMessage(
+    //             new Notification(title, message, type, null, onClick)));
+    // }
+}
