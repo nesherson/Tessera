c65ba48aed442ec08e728586f597b47ecf5b60c3
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-03-05 23:46:25 +0100

    Add hit test and move to ellipse and rectangle shapes

--- src/Tessera.App/Models/Shapes/EllipseShape.cs
+++ src/Tessera.App/Models/Shapes/EllipseShape.cs
@@ -6,4 +6,15 @@ public class EllipseShape : ShapeBase
     {
         return rect.Intersects(new Rect(X, Y, Width, Height));
     }
+
+    public override bool HitTest(Point worldPoint, double tolerance)
+    {
+        return new Rect(X, Y, Width, Height).Contains(worldPoint);
+    }
+
+    public override void Move(Vector delta)
+    {
+        X += delta.X;
+        Y += delta.Y;
+    }
 }
--- src/Tessera.App/Models/Shapes/RectangleShape.cs
+++ src/Tessera.App/Models/Shapes/RectangleShape.cs
@@ -1,9 +1,22 @@
-﻿namespace Tessera.App.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Tessera.App.Models;
 
 public class RectangleShape : ShapeBase
 {
     public override bool Intersects(Rect rect)
     {
         return rect.Intersects(new Rect(X, Y, Width, Height));
+    }
+
+    public override bool HitTest(Point worldPoint, double tolerance)
+    {
+        return new Rect(X, Y, Width, Height).Contains(worldPoint);
+    }
+
+    public override void Move(Vector delta)
+    {
+        X += delta.X;
+        Y += delta.Y;
     }
 }
