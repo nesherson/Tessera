12f0a1c0ef34bc8ae044ff93f11bf9987ed1b05f
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-03-17 22:53:47 +0100

    Add MultiShapePropertyProxy for shape editing

---
+++ src/Tessera.App/Models/MultiShapePropertyProxy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Tessera.App.Interfaces;
+
+namespace Tessera.App.Models;
+
+public class MultiShapePropertyProxy : ObservableObject, IShapeProperties
+{
+    private readonly List<ShapeBase> _shapes;
+
+    public MultiShapePropertyProxy(List<ShapeBase> shapes)
+    {
+        _shapes = shapes;
+    }
+
+    public IBrush StrokeColor
+    {
+        get => _shapes.FirstOrDefault()?.StrokeColor ?? Brushes.Black;
+        set
+        {
+            foreach (var shape in _shapes)
+                shape.StrokeColor = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public double StrokeThickness
+    {
+        get => _shapes.FirstOrDefault()?.StrokeThickness ?? 1;
+        set
+        {
+            foreach (var shape in _shapes)
+                shape.StrokeThickness = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public double Opacity
+    {
+        get => _shapes.FirstOrDefault()?.Opacity ?? 1;
+        set
+        {
+            foreach (var shape in _shapes)
+                shape.Opacity = value;
+            OnPropertyChanged();
+        }
+    }
+}
