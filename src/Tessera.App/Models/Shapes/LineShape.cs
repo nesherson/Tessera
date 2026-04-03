a75dc0fc683e9ef3654007583d93d69c303c76f8
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-04-03 17:43:04 +0200

    Add right, left and bottom resizing to the line shape

--- src/Tessera.App/Models/Shapes/LineShape.cs
+++ src/Tessera.App/Models/Shapes/LineShape.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Collections;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Tessera.App.Enumerations;
 using Tessera.App.Helpers;
 using Tessera.App.Interfaces;
 
@@ -7,6 +8,8 @@ namespace Tessera.App.Models;
 
 public partial class LineShape : ShapeBase
 {
+    private const double MinWidth = 20;
+
     [ObservableProperty]
     private Point _startPoint;
 
@@ -15,7 +18,8 @@ public partial class LineShape : ShapeBase
 
     public override bool Intersects(Rect rect)
     {
-        if (rect.Contains(StartPoint) || rect.Contains(EndPoint)) return true;
+        if (rect.Contains(StartPoint) || rect.Contains(EndPoint))
+            return true;
 
         var topLeft = rect.TopLeft;
         var topRight = rect.TopRight;
@@ -39,12 +43,65 @@ public partial class LineShape : ShapeBase
         EndPoint = new Point(EndPoint.X + delta.X, EndPoint.Y + delta.Y);
     }
 
+    public override void Scale(ResizePoint resizePoint, Vector delta)
+    {
+        switch (resizePoint)
+        {
+            case ResizePoint.TopLeft:
+            {
+                break;
+            }
+            case ResizePoint.TopRight:
+                break;
+            case ResizePoint.BottomLeft:
+                break;
+            case ResizePoint.BottomRight:
+                break;
+            case ResizePoint.Bottom:
+            {
+                if (StartPoint.Y > EndPoint.Y)
+                {
+                    var newY = Math.Max(StartPoint.Y + delta.Y, EndPoint.Y + MinWidth);
+                    
+                    StartPoint = new Point(StartPoint.X, newY);
+                }
+                else
+                {
+                    var newY = Math.Max(EndPoint.Y + delta.Y, StartPoint.Y + MinWidth);
+                    
+                    EndPoint = new Point(EndPoint.X, newY);
+                }
+                
+                break;
+            }
+            case ResizePoint.Left:
+            {
+                var newX = Math.Min(StartPoint.X + delta.X, EndPoint.X - MinWidth);
+
+                StartPoint = new Point(newX, StartPoint.Y);
+
+                break;
+            }
+            case ResizePoint.Right:
+            {
+                var newX = Math.Max(EndPoint.X + delta.X, StartPoint.X + MinWidth);
+
+                EndPoint = new Point(newX, EndPoint.Y);
+
+                break;
+            }
+            case ResizePoint.Top:
+                break;
+        }
+    }
+
     public override Rect GetBounds()
     {
         var x = Math.Min(StartPoint.X, EndPoint.X);
         var y = Math.Min(StartPoint.Y, EndPoint.Y);
         var width = Math.Abs(StartPoint.X - EndPoint.X);
-        var height = Math.Abs(StartPoint.Y - EndPoint.Y);;
+        var height = Math.Abs(StartPoint.Y - EndPoint.Y);
+        ;
 
         return InflateForStroke(new Rect(x, y, width, height));
     }
