fd457f152d0dd66d610efd11af912640e635e34b
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-04-06 22:07:33 +0200

    Add full resizing functionality to the triangle shape

--- src/Tessera.App/Models/Shapes/TriangleShape.cs
+++ src/Tessera.App/Models/Shapes/TriangleShape.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Tessera.App.Enumerations;
@@ -14,20 +15,19 @@ public partial class TriangleShape : ShapeBase
     public override bool Intersects(Rect rect)
     {
         if (Points.Any(rect.Contains)) return true;
-        
+
         var a = Points[0];
         var b = Points[1];
         var c = Points[2];
-        
-        if (rect.Contains(a) || rect.Contains(b) || rect.Contains(c))
+
+        if (rect.Contains(a) || rect.Contains(b) || rect.Contains(c)) return true;
+
+        if (GeometryHelpers.PointInTriangle(rect.TopLeft, a, b, c) ||
+            GeometryHelpers.PointInTriangle(rect.TopRight, a, b, c) ||
+            GeometryHelpers.PointInTriangle(rect.BottomLeft, a, b, c) ||
+            GeometryHelpers.PointInTriangle(rect.BottomRight, a, b, c))
             return true;
-        
-        if (GeometryHelpers.PointInTriangle(rect.TopLeft, a, b, c)
-            || GeometryHelpers.PointInTriangle(rect.TopRight, a, b, c)
-            || GeometryHelpers.PointInTriangle(rect.BottomLeft, a, b, c)
-            || GeometryHelpers.PointInTriangle(rect.BottomRight, a, b, c))
-            return true;
-        
+
         Point[] rectCorners = [rect.TopLeft, rect.TopRight, rect.BottomRight, rect.BottomLeft];
         Point[] triEdges = [a, b, c, a];
 
@@ -35,9 +35,8 @@ public partial class TriangleShape : ShapeBase
         {
             for (var j = 0; j < 4; j++)
             {
-                if (GeometryHelpers.SegmentsIntersect(
-                        triEdges[i], triEdges[i + 1],
-                        rectCorners[j], rectCorners[(j + 1) % 4]))
+                if (GeometryHelpers.SegmentsIntersect(triEdges[i], triEdges[i + 1], rectCorners[j],
+                        rectCorners[(j + 1) % 4]))
                     return true;
             }
         }
@@ -51,9 +50,9 @@ public partial class TriangleShape : ShapeBase
         var secondPoint = Points[1];
         var thirdPoint = Points[2];
 
-        return GeometryHelpers.Cross(firstPoint, secondPoint, worldPoint) <= 0
-               && GeometryHelpers.Cross(secondPoint, thirdPoint, worldPoint) <= 0
-               && GeometryHelpers.Cross(thirdPoint, firstPoint, worldPoint) <= 0;
+        return GeometryHelpers.Cross(firstPoint, secondPoint, worldPoint) <= 0 &&
+               GeometryHelpers.Cross(secondPoint, thirdPoint, worldPoint) <= 0 &&
+               GeometryHelpers.Cross(thirdPoint, firstPoint, worldPoint) <= 0;
     }
 
     public override void Move(Vector delta) =>
@@ -62,8 +61,7 @@ public partial class TriangleShape : ShapeBase
 
     public override Rect GetBounds()
     {
-        if (Points.Count == 0)
-            return default;
+        if (Points.Count == 0) return default;
 
         var minX = Points.Min(p => p.X);
         var minY = Points.Min(p => p.Y);
@@ -77,18 +75,120 @@ public partial class TriangleShape : ShapeBase
     {
         switch (resizePoint)
         {
+            case ResizePoint.TopLeft:
+            {
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
+                
+                var newLeftPoint = new Point(
+                    Math.Min(leftPoint.X + delta.X, rightPoint.X - 20),
+                    leftPoint.Y);
+                var newMidPoint = new Point((newLeftPoint.X + rightPoint.X) / 2, Math.Min(midPoint.Y + delta.Y, leftPoint.Y - 20));
+                
+                Points = new ObservableCollection<Point>([newLeftPoint, newMidPoint, rightPoint]);
+                
+                break;
+            }
+            case ResizePoint.TopRight:
+            {
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
+                
+                var newRightPoint = new Point(
+                    Math.Max(rightPoint.X + delta.X, leftPoint.X + 20),
+                    rightPoint.Y);
+                var newMidPoint = new Point((leftPoint.X + newRightPoint.X) / 2, Math.Min(midPoint.Y + delta.Y, leftPoint.Y - 20));
+                
+                Points = new ObservableCollection<Point>([leftPoint, newMidPoint, newRightPoint]);
+                
+                break;
+            }
+            case ResizePoint.BottomLeft:
+            {
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
+                
+                var newLeftPoint = new Point(
+                    Math.Min(leftPoint.X + delta.X, rightPoint.X - 20),
+                    Math.Max(leftPoint.Y + delta.Y, midPoint.Y + 20));
+                var newRightPoint = new Point(
+                    rightPoint.X,
+                    Math.Max(rightPoint.Y + delta.Y, midPoint.Y + 20));
+                var newMidPoint = new Point((newLeftPoint.X + newRightPoint.X) / 2, midPoint.Y);
+                
+                Points = new ObservableCollection<Point>([newLeftPoint, newMidPoint, newRightPoint]);
+                break;
+            }
+            case ResizePoint.BottomRight:
+            {
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
+
+                var newRightPoint = new Point(
+                    Math.Max(rightPoint.X + delta.X, leftPoint.X + 20),
+                    Math.Max(rightPoint.Y + delta.Y, midPoint.Y + 20));
+                var newLeftPoint = new Point(
+                    leftPoint.X,
+                    Math.Max(rightPoint.Y + delta.Y, midPoint.Y + 20));
+                var newMidPoint = new Point((newLeftPoint.X + newRightPoint.X) / 2, midPoint.Y);
+                
+                Points = new ObservableCollection<Point>([newLeftPoint, newMidPoint, newRightPoint]);
+                
+                break;
+            }
+            case ResizePoint.Left:
+            {
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
+
+                var newLeftPoint = new Point(Math.Min(leftPoint.X + delta.X, rightPoint.X - 20), leftPoint.Y);
+                var newMidPoint = new Point((newLeftPoint.X + rightPoint.X) / 2, midPoint.Y);
+
+                Points = new ObservableCollection<Point>([newLeftPoint, newMidPoint, rightPoint]);
+
+                break;
+            }
+            case ResizePoint.Top:
+            {
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
+
+                var newMidPoint = new Point(midPoint.X, Math.Min(midPoint.Y + delta.Y, leftPoint.Y - 20));
+
+                Points = new ObservableCollection<Point>([leftPoint, newMidPoint, rightPoint]);
+
+                break;
+            }
             case ResizePoint.Right:
             {
-                var mostRightPoint = Points.MaxBy(p => p.X);
-                var newRightPoint = new Point(mostRightPoint.X + delta.X, mostRightPoint.Y);
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
 
-                var newPoints = Points.Where(p => p != mostRightPoint).ToList();
-                
-                newPoints.Add(newRightPoint);
-                
-                Points = new ObservableCollection<Point>(newPoints);
-               
-                
+                var newRightPoint = new Point(Math.Max(rightPoint.X + delta.X, leftPoint.X + 20), rightPoint.Y);
+                var newMidPoint = new Point((leftPoint.X + newRightPoint.X) / 2, midPoint.Y);
+
+                Points = new ObservableCollection<Point>([leftPoint, newMidPoint, newRightPoint]);
+
+                break;
+            }
+            case ResizePoint.Bottom:
+            {
+                var rightPoint = Points.MaxBy(p => p.X);
+                var leftPoint = Points.MinBy(p => p.X);
+                var midPoint = Points.First(p => p.X > leftPoint.X && p.X < rightPoint.X);
+
+                var newLeftPoint = new Point(leftPoint.X, Math.Max(leftPoint.Y + delta.Y, midPoint.Y + 20));
+                var newRightPoint = new Point(rightPoint.X, Math.Max(rightPoint.Y + delta.Y, midPoint.Y + 20));
+
+                Points = new ObservableCollection<Point>([newLeftPoint, midPoint, newRightPoint]);
+
                 break;
             }
         }
