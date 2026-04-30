0d8b1d5bdc736af66c84e1419245234384ad0af2
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-04-30 23:47:29 +0200

    Remove point tool and settings

--- src/Tessera.App/ViewModels/PageViewModels/DrawingPageViewModel.cs
+++ src/Tessera.App/ViewModels/PageViewModels/DrawingPageViewModel.cs
@@ -57,7 +57,6 @@ public partial class DrawingPageViewModel : PageViewModel, ICanvasContext
 
     public DrawingPageViewModel()
     {
-        var pointShapeSettings = new PointShapeToolSettings();
         var lineShapeSettings = new LineShapeToolSettings();
         var shapeSettings = new ShapeToolSettings();
         var polylineShapeSettings = new PolylineShapeToolSettings();
@@ -82,14 +81,6 @@ public partial class DrawingPageViewModel : PageViewModel, ICanvasContext
                 IconPath = Icons.HandGrabbing,
                 Tool = new PanTool(this),
                 Shortcut = new KeyGesture(Key.H)
-            },
-            new ToolItem
-            {
-                Name = "Point",
-                IconPath = Icons.Point,
-                Tool = new PointShapeTool(this, pointShapeSettings),
-                ToolSettings = pointShapeSettings,
-                Shortcut = new KeyGesture(Key.P)
             },
             new ToolItem
             {
